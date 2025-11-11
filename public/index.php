587237541b32fa05b77bcc0094531730ecc1398e
khamosh464101 <azim@momtaz.af>
2025-11-11 04:03:54 -0500

    11 november zero one, 2025

--- public/index.php
+++ public/index.php
@@ -1,5 +1,18 @@
 <?php
 
+// CORS headers for cPanel compatibility
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, PATCH");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, X-CSRF-TOKEN");
+header("Access-Control-Expose-Headers: Content-Length, Content-Range");
+header("Access-Control-Allow-Credentials: true");
+
+// Handle preflight OPTIONS request
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
